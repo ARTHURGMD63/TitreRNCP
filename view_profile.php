6526a2a6f69181a2139bbabc078aec9fbc896589
Arthur <arthur@example.com>
2026-04-23 15:56:17 +0200

    Remove SPA router, back to standard navigation

--- view_profile.php
+++ view_profile.php
@@ -226,7 +226,7 @@ $upcomingEvents = $stmtEv->fetchAll();
 </div>
 
 <div class="toast" id="toast"></div>
-<script src="/assets/js/router.js"></script>
+
 <script src="/assets/js/app.js"></script>
 </body>
 </html>
